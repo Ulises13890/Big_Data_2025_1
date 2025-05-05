18e6a5d9322921d7476ed79c7d807d3f68856366
Udeilor Ulises Uscategui Puentes <35670319+Ulises13890@users.noreply.github.com>
2025-05-05 14:48:28 -0500

    Create setup.py
    Setup de dependencias

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="NFTPolGames",
+    author="Udeilor Uscategui",
+    author_email="udeilor.uscategui@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+)
