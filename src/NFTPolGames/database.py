cd9122b8553aa59094541050f04c079512cc5830
Udeilor Ulises Uscategui Puentes <35670319+Ulises13890@users.noreply.github.com>
2025-05-16 17:07:50 -0500

    update matic_database

--- src/NFTPolGames/database.py
+++ src/NFTPolGames/database.py
@@ -9,7 +9,7 @@ class DataBase:
         self.db_name = "src/NFTPolGames/static/db/matic_pol_historical.sqlite"
     
     # CRUD C = create(insert) R= read U = update DF= Delete
-    def insert_data(self,df = pd.DataFrame(),nom_table="dolar_analisis"):
+    def insert_data(self,df = pd.DataFrame(),nom_table="matic_analisis"):
         try:
             df = df.copy()
             conn = sqlite3.connect(self.db_name)
@@ -31,3 +31,36 @@ class DataBase:
         except Exception as errores:
             print("error al obtener los datos")
             return df
+            
+      def update_data(self, nom_table="", data={}, condition=""):
+        try:
+            if len(nom_table) > 0 and len(data) > 0 and len(condition) > 0:
+                conn = sqlite3.connect(self.db_name)
+                cursor = conn.cursor()
+                set_values = ", ".join([f"{key} = ?" for key in data.keys()])
+                query = f"UPDATE {nom_table} SET {set_values} WHERE {condition}"
+                cursor.execute(query, tuple(data.values()))
+                conn.commit()
+                print(f"*************** Datos actualizados en la tabla: {nom_table} con la condición: {condition}*********")
+                cursor.close()
+                conn.close()
+            else:
+                print("Error: Nombre de tabla, datos a actualizar y condición son obligatorios para la actualización.")
+        except Exception as errores:
+            print("Error al actualizar los datos:", errores)
+
+    def delete_data(self, nom_table="", condition=""):
+        try:
+            if len(nom_table) > 0 and len(condition) > 0:
+                conn = sqlite3.connect(self.db_name)
+                cursor = conn.cursor()
+                query = f"DELETE FROM {nom_table} WHERE {condition}"
+                cursor.execute(query)
+                conn.commit()
+                print(f"*************** Datos eliminados de la tabla: {nom_table} con la condición: {condition}*********")
+                cursor.close()
+                conn.close()
+            else:
+                print("Error: Nombre de tabla y condición son obligatorios para la eliminación.")
+        except Exception as errores:
+            print("Error al eliminar los datos:", errores)
